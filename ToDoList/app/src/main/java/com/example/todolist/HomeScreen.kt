b1f742a74c8cb9582ade15fd6170e0cac5179e2a
brunocamposfonseca <brunocamposfonsecaofc@gmail.com>
2025-06-17 15:45:28 -0300

    changes on ToDoList

--- ToDoList/app/src/main/java/com/example/todolist/HomeScreen.kt
+++ ToDoList/app/src/main/java/com/example/todolist/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.example.todolist
 
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,8 +11,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Button
@@ -20,6 +26,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -47,26 +54,46 @@ fun Modifier.bottomBorder(color: Color, strokeWidth: Float) = drawBehind {
     )
 }
 
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 @Composable
 fun HomeScreen(navController: NavController){
     var todo by remember { mutableStateOf("") }
+    var statusMenu by remember { mutableStateOf(true) }
+    var listaTarefa = remember { mutableStateListOf<String>() }
+    var tarefaConcluida = remember { mutableStateListOf<String>() }
+
 
     Column {
         Row (modifier = Modifier.fillMaxWidth().background(Color(0xFF6650a4)),) {
-            Text(
-                "Tarefas",
-                color = Color(0xFFFFFFFF),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.SemiBold,
-                fontFamily = FontFamily.SansSerif,
-                modifier = Modifier.padding(18.dp)
-            )
+            Row (modifier = Modifier.padding(20.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    "Tarefas",
+                    color = Color(0xFFFFFFFF),
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 26.sp
+                )
+
+                Button(
+                    onClick = { navController.navigate("login") },
+                    shape = RoundedCornerShape(5.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFbf0b0b), contentColor = Color(0xFFFFFFFF))
+                ) {
+                    Row (horizontalArrangement = Arrangement.spacedBy(5.dp), verticalAlignment = Alignment.CenterVertically) {
+                        Icon(Icons.Default.ExitToApp, contentDescription = null)
+                        Text("Sair")
+                    }
+                }
+
+            }
+
+
         }
         Column (modifier = Modifier.padding(20.dp)){
             OutlinedTextField(
                 value = todo,
                 onValueChange = { newTodo ->
-                    todo = newTodo.trim()
+                    todo = newTodo
                 },
                 modifier = Modifier.fillMaxWidth(),
                 label = { Text(text = "Digite sua tarefa... ", fontSize = 16.sp, fontWeight = FontWeight.Medium) },
@@ -88,7 +115,13 @@ fun HomeScreen(navController: NavController){
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Button(
-                    onClick = {},
+                    onClick = {
+                        if(todo.isNotEmpty()){
+                            listaTarefa.addFirst(todo)
+                            todo = ""
+                        }
+                        Log.d("Lista: ", listaTarefa.toString())
+                    },
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color(0xFF6650a4),
                         contentColor = Color(0xFFFFFFFF)
@@ -102,7 +135,9 @@ fun HomeScreen(navController: NavController){
                 }
 
                 Button(
-                    onClick = {},
+                    onClick = {
+                        todo = ""
+                    },
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color(0xFFbf0b0b),
                         contentColor = Color(0xFFFFFFFF)
@@ -118,9 +153,12 @@ fun HomeScreen(navController: NavController){
 
             Row (
                 modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Box(modifier = Modifier.bottomBorder(color = Color(0xFF6650a4), strokeWidth = 6.0F)) {
+                Box(modifier = Modifier.then(if (statusMenu) Modifier.bottomBorder(color = Color(0xFF6650a4), strokeWidth = 6.0F) else Modifier).clickable {
+                        statusMenu = !statusMenu
+                    }
+                ) {
                     Text(
                         "Tarefas a Fazer",
                         fontWeight = FontWeight.Bold,
@@ -129,112 +167,78 @@ fun HomeScreen(navController: NavController){
                     )
                 }
 
-                Box(modifier = Modifier.padding(10.dp)) {
+                Box(modifier = Modifier.then(if (!statusMenu) Modifier.bottomBorder(color = Color(0xFF6650a4), strokeWidth = 6.0F) else Modifier).clickable {
+                        statusMenu = !statusMenu
+                    }
+                ) {
                     Text(
                         "Tarefas Concluidas",
                         fontWeight = FontWeight.Bold,
-                        fontSize = 18.sp
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(10.dp)
                     )
                 }
             }
 
-            Column (
-                modifier = Modifier.fillMaxWidth().padding(top = 18.dp),
-                verticalArrangement = Arrangement.spacedBy(20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+            LazyColumn (
+                modifier = Modifier.fillMaxWidth().padding(top = 18.dp).padding(horizontal = 10.dp),
+                verticalArrangement = Arrangement.spacedBy(30.dp),
+                horizontalAlignment = Alignment.Start
             ) {
-                Row (horizontalArrangement = Arrangement.spacedBy(14.dp)) {
-                    Text(
-                        "1 - Criar Wireframe de Home",
-                        fontWeight = FontWeight.Medium,
-                        color = Color(0xFF666666)
-                    )
-                    Row (horizontalArrangement = Arrangement.spacedBy(6.dp)){
-                        Icon(
-                            Icons.Outlined.Delete,
-                            contentDescription = null,
-                            tint = Color(0xFFbf0b0b),
-                            modifier = Modifier.clickable {  }
-                        )
+                if (statusMenu){
+                    itemsIndexed(listaTarefa) { index, item ->
+                        Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                "${index+1} - $item",
+                                fontWeight = FontWeight.Medium,
+                                color = Color(0xFF666666)
+                            )
+                            Row (horizontalArrangement = Arrangement.spacedBy(6.dp)){
+                                Icon(
+                                    Icons.Outlined.Delete,
+                                    contentDescription = null,
+                                    tint = Color(0xFFbf0b0b),
+                                    modifier = Modifier.clickable {
+                                        listaTarefa.removeAt(index)
+                                    }
+                                )
 
-                        Icon(
-                            Icons.Outlined.Check,
-                            contentDescription = null,
-                            tint = Color(0xFF14bf0b),
-                            modifier = Modifier.clickable {  }
-                        )
+                                Icon(
+                                    Icons.Outlined.Check,
+                                    contentDescription = null,
+                                    tint = Color(0xFF14bf0b),
+                                    modifier = Modifier.clickable {
+                                        tarefaConcluida.add(item)
+                                        listaTarefa.removeAt(index)
+                                    }
+                                )
+                            }
+                        }
                     }
-
-                }
-                Row (horizontalArrangement = Arrangement.spacedBy(14.dp)) {
-                    Text(
-                        "2 - Treinar Go Lang",
-                        fontWeight = FontWeight.Medium,
-                        color = Color(0xFF666666)
-                    )
-                    Row (horizontalArrangement = Arrangement.spacedBy(6.dp)){
-                        Icon(
-                            Icons.Outlined.Delete,
-                            contentDescription = null,
-                            tint = Color(0xFFbf0b0b),
-                            modifier = Modifier.clickable {  }
-                        )
-
-                        Icon(
-                            Icons.Outlined.Check,
-                            contentDescription = null,
-                            tint = Color(0xFF14bf0b),
-                            modifier = Modifier.clickable {  }
-                        )
+                } else {
+                    itemsIndexed(tarefaConcluida) { index, item ->
+                        Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                "${index+1} - $item",
+                                fontWeight = FontWeight.Medium,
+                                color = Color(0xFF666666),
+                                fontSize = 16.sp
+                            )
+                            Row (horizontalArrangement = Arrangement.spacedBy(6.dp)){
+                                Icon(
+                                    Icons.Outlined.Delete,
+                                    contentDescription = null,
+                                    tint = Color(0xFFbf0b0b),
+                                    modifier = Modifier.clickable {
+                                        tarefaConcluida.removeAt(index)
+                                    }
+                                )
+                            }
+                        }
                     }
-
                 }
-                Row (horizontalArrangement = Arrangement.spacedBy(14.dp)) {
-                    Text(
-                        "3 - Estudar Mais um pouco",
-                        fontWeight = FontWeight.Medium,
-                        color = Color(0xFF666666)
-                    )
-                    Row (horizontalArrangement = Arrangement.spacedBy(6.dp)){
-                        Icon(
-                            Icons.Outlined.Delete,
-                            contentDescription = null,
-                            tint = Color(0xFFbf0b0b),
-                            modifier = Modifier.clickable {  }
-                        )
 
-                        Icon(
-                            Icons.Outlined.Check,
-                            contentDescription = null,
-                            tint = Color(0xFF14bf0b),
-                            modifier = Modifier.clickable {  }
-                        )
-                    }
 
-                }
-                Row (horizontalArrangement = Arrangement.spacedBy(14.dp)) {
-                    Text(
-                        "4 - Enviar protótipo para o cliente",
-                        fontWeight = FontWeight.Medium,
-                        color = Color(0xFF666666)
-                    )
-                    Row (horizontalArrangement = Arrangement.spacedBy(6.dp)){
-                        Icon(
-                            Icons.Outlined.Delete,
-                            contentDescription = null,
-                            tint = Color(0xFFbf0b0b),
-                            modifier = Modifier.clickable {  }
-                        )
-
-                        Icon(
-                            Icons.Outlined.Check,
-                            contentDescription = null,
-                            tint = Color(0xFF14bf0b),
-                            modifier = Modifier.clickable {  }
-                        )
-                    }
-
-                }
             }
         }
     }
